43df5f366560b485005255122b6db6c80f41204e
ElenaNovikova1978 <languagebridge@mail.ru>
2024-05-19 14:06:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+name ="price_per_kg"
+print(name)
+print (34)
+name="weight"
+print (name)
+print (4.5)
+name = "money"
+print (name)
+print (200)
+name="change"
+print (name)
+print (200-34*4.5)
+name='plumps_price_per_kg'
+print(name)
+print (54)
+name='weight'
+print(name)
+print (5.5)
+name="money"
+print(name)
+print (1000)
+name='change'
+print(name)
+print (1000-54*5.5)
+name='<Product Name>'
+print (name)
+name ='<Weight> kg'
+print(name)
+name="<Price> rub/kg"
+print(name)
+name="<Total>rub"
+print(name)
+name='<Paid> rub'
+print (name)
+name='<Change>rub'
+print(name)
+name="Купи конструктор!"
+print (name)
+print (name*5)
+print (7)
+name ="'<любимое дело>'!"
+print (name)
+print (7*name)
